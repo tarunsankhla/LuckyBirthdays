f85595d7e5ea306f253ad463a850a85f09fa88c4
Tether <tarun.sankhla.prc@gmail.com>
2021-10-03 15:25:18 +0530

    scrit added to dates

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ const birthdate = document.querySelector("#date");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#checkbtn");
 
-
-
-checkButton.addEventListener('click',function getValues(){
-    console.log(birthdate.value,luckyNumber.value)
-})
+function calculatesum(Birthdate){
+    let sum = 0;
+    Birthdate = Birthdate.replaceAll("-","");
+    console.log(Birthdate);
+    for(let index=0;index<Birthdate.length;index++){
+        sum = sum + Number(Birthdate.charAt(index));
+        // console.log(Birthdate.charAt(index));
+    }
+    // for(var i in  [1,6,4])console.log(i)
+    //return index and iterates over the object
+    // for(var i of [1,6,4])console.log(i)
+    //iterate over value and  is dependent on the type of iterable object.
+    //loop over any type of data that is iterable.
+    return sum;
+}
+function checknumberIsLucky(){
+    
+    const Birthdate = birthdate.value;
+    const luckynumber = luckyNumber.value;
+    const sum = calculatesum(Birthdate);
+    console.log(sum);
+}
+checkButton.addEventListener('click',checknumberIsLucky );
